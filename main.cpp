1bf8c7456fa9046362e1e11ce5d02f5722db3e96
Kalana Wijegunarathna <kalanainduwara777@gmail.com>
2020-04-30 02:46:09 +0530

    writing into output file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <time.h>
 #include <fstream>
+#include <iterator>
 #include "vectorizer.h"
 
 using namespace std;
@@ -30,13 +31,28 @@ void split_n_count(const char *raw_seq, int n,int length_mean,int length_stdev,i
 
 int main() {
     std::string str;
-    ifstream file ("seq.txt");
+    ifstream file ("sequence.fasta");
     std::getline(file,str);
     const char* c = str.c_str();
 
-    split_n_count(c,str.length(),3,1,20);
+    split_n_count(c,str.length(),1500,150,1000);
 
+    ofstream myfile;
+    myfile.open ("./output.txt");
+    myfile << str.length() << "\n";
+    for(int i=0; i < fi.size(); i++){
+
+        for(int k=0; k < sizeof(fi[i].freqs)/sizeof(fi[i].freqs[0]); k++){
+            myfile << fi[i].freqs[k] << "\n";
+        }
+    }
+
+    myfile.close();
+
+   
     int n;
     n++;
+
+     
     return 0;
 }
